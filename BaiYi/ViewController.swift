045285a9e75e6f4b3b0f26c1c7d777b7694c4518
skerwin <skerwin@163.com>
2020-07-23 10:54:24 +0900

    Initial Commit

---
+++ BaiYi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiYi
+//
+//  Created by 脇山　和茂 on 2020/07/23.
+//  Copyright © 2020 gansukanglin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
